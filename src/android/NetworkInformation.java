20a5f350855f454df714a7663c0de3401bcad9d9
Pablo Ortiz <portizb@gmail.com>
2016-09-22 17:54:21 +0200

    Update NetworkInformation.java

--- src/android/NetworkInformation.java
+++ src/android/NetworkInformation.java
@@ -4,21 +4,43 @@ import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.util.Log;
+
+import com.telefonica.movistar.net.GatewayResolver;
+import com.telefonica.movistar.net.GatewayAddressInfo;
+
 public class NetworkInformation extends CordovaPlugin {
+
+    private static final String LOG_TAG = "NetworkInformation";
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals("lookup")) {
 
-            String name = data.getString(0);
-            String message = "Lookup " + name;
-            callbackContext.success(message);
+            try {
 
-            return true;
+                String ip = data.getString(0);
+                String message = "Lookup " + ip;
+
+                GatewayAddressInfo gwAddress = GatewayResolver.lookup("172.26.23.22");
+
+                if (gwAddress != null)
+                    message = "Gateway " +  gwAddress.getIpAddress().getHostAddress() + " for ip " + ip;
+                else
+                    message = "Gateway not found for ip " + ip;
+
+                callbackContext.success(message);
+                return true;
+
+            } catch (Exception e) {
+                callbackContext.error ("Fatal error: " + e.toString());
+                return false;
+            }
 
         } else {
 
+            callbackContext.error ("Invalid command");
             return false;
 
         }

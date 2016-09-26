2ff7563246ac521f1d21641f5d6f0743b2c41004
Pablo Ortiz <portizb@gmail.com>
2016-09-26 17:14:00 +0200

    Update netinfo.js

--- www/netinfo.js
+++ www/netinfo.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     lookup: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "NetworkInfo", "lookup");
+        cordova.exec(successCallback, errorCallback, "NetworkInfoPlugin", "lookup");
     }
 };

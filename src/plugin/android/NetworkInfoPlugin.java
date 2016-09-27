0048e00cae849a4e7f3cd9d4e36338fb05e2798a
Pablo Ortiz <portizb@gmail.com>
2016-09-27 10:38:20 +0200

    Create NetworkInfoPlugin.java

---
+++ src/plugin/android/NetworkInfoPlugin.java
@@ -0,0 +1,41 @@
+package com.telefonica.movistar.cordova.plugin;
+
+import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import android.util.Log;
+
+import com.telefonica.movistar.net.NetworkInfo;
+import com.telefonica.movistar.net.NetworkInfoProvider;
+
+public class NetworkInfoPlugin extends CordovaPlugin {
+
+    private static final String LOG_TAG = "NetworkInfoPlugin";
+
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
+        if (action.equals("lookup")) {
+
+            try {
+
+                NetworkInfo networkInfo = NetworkInfoProvider.getNetworkInfo();
+                String message = (networkInfo == null) ? "Network Information not found" : networkInfo.toString();
+                
+                callbackContext.success(message);
+                return true;
+
+            } catch (Exception e) {
+                callbackContext.error ("Fatal error: " + e.toString());
+                return false;
+            }
+
+        } else {
+
+            callbackContext.error ("Invalid command");
+            return false;
+
+        }
+    }
+}

e68acd099f0b6b755374a1785d6eb87b2d4ef4f1
Pablo Ortiz <portizb@gmail.com>
2016-09-27 15:24:39 +0200

    Create NetworkInfoProvider.java

---
+++ src/android/NetworkInfoLib/src/com/movistar/tvsindesco/net/NetworkInfoProvider.java
@@ -0,0 +1,15 @@
+package com.telefonica.movistar.net;
+
+/**
+* Created by pablo on 14/09/16.
+*/
+
+public class NetworkInfoProvider {
+  private static final String ipAddressLookup = "172.26.23.22";
+  
+  public static NetworkInfo getNetworkInfo () throws Exception {
+    return new NetworkInfo(IpConverter.toInetAddress("192.168.1.38"),
+          IpConverter.toInetAddress("192.168.1.1"),
+          IpConverter.toInetAddress(subnetAddr), -255);
+    }
+}

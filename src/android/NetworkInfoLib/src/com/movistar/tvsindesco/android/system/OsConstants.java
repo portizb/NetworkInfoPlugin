42c4589e1ac22c69140667f71aa2ffc0a60ba583
Pablo Ortiz <portizb@gmail.com>
2016-09-28 15:49:36 +0200

    Create OsConstants.java

---
+++ src/android/NetworkInfoLib/src/com/movistar/tvsindesco/android/system/OsConstants.java
@@ -0,0 +1,26 @@
+package com.movistar.tvsindesco.android.system;
+
+/**
+ * Created by pablo on 5/09/16.
+ */
+
+public final class OsConstants {
+    public static final int AF_UNSPEC = 0;
+    public static final int AF_INET = 2;
+    public static final int AF_INET6 = 10;
+    public static final int AF_NETLINK = 16;
+
+    public static final int SOCK_DGRAM = 2;
+    public static final int SOCK_RAW = 3;
+
+    public static final int SOL_SOCKET = 1;
+
+    public static final int SO_RCVBUF = 8;
+    public static final int SO_RCVTIMEO = 20;
+    public static final int SO_SNDTIMEO = 21;
+
+    public static final int _SC_CLK_TCK = 0x0006;
+
+    public static final int NETLINK_ROUTE = 0;
+
+}
